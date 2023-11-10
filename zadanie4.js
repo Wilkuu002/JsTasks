c34187c0a3298e8a0fa897b6d853b3980cb33999
Wilkuu002 <Kacperwilk@poczta.onet.pl>
2023-11-10 13:35:02 +0100

    10.11

---
+++ zadanie4.js
@@ -0,0 +1,10 @@
+var countBits = function (n) {
+  const tablica = n.toString(2).split("");
+  var liczba = 0;
+  tablica.forEach((element) => {
+    if (element == 1) {
+      liczba++;
+    }
+  });
+  return liczba;
+};
