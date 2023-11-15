c242d96450e8e5f76c9eca0a3cf292f427de9c17
Wilkuu002 <Kacperwilk@poczta.onet.pl>
2023-11-16 00:14:24 +0100

    16.11

--- zadanie11.js
+++ zadanie11.js
@@ -1,3 +1,13 @@
-function findUniq(arr) {
-  // do magic
+function findUniq(tablica) {
+  // Sortuj tablicę, aby identyfikować różnice między liczbami
+  tablica.sort(function (a, b) {
+    return a - b;
+  });
+
+  // Porównaj pierwszy i drugi element, aby znaleźć unikatową wartość
+  if (tablica[0] !== tablica[1]) {
+    return tablica[0];
+  } else {
+    return tablica[tablica.length - 1];
+  }
 }
