27e672444df91348d88b27167b5a716be295a5d9
Wilkuu002 <Kacperwilk@poczta.onet.pl>
2023-11-27 12:41:49 +0100

    27.11

--- zadanie15.js
+++ zadanie15.js
@@ -1,3 +1,16 @@
-function listSquared(m, n) {
-  // your code
+function rgb(r, g, b) {
+  let r16 = r.toString(16);
+  let g16 = g.toString(16);
+  let b16 = b.toString(16);
+  tablica = [r16, g16, b16];
+  for (let i = 0; i < tablica.length; i++) {
+    if (parseInt(tablica[i], 16) < 0) {
+      tablica[i] = "00";
+    } else if (parseInt(tablica[i], 16) > "255") {
+      tablica[i] = "ff";
+    } else if (tablica[i].length < 2) {
+      tablica[i] = "0" + tablica[i];
+    }
+  }
+  return tablica.join("").toUpperCase();
 }
