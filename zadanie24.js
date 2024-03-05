88a73dc9c28fc328ff628f642f93c9cd1dde6a95
Wilkuu002 <Kacperwilk@poczta.onet.pl>
2024-03-05 03:14:02 +0100

    zadaniejs 05.03

--- zadanie24.js
+++ zadanie24.js
@@ -1,3 +1,31 @@
-var maxSequence = function(arr){
+// Returns number of complete beeramid levels
+var beeramid = function(bonus, price) {
+    let ilePuszek = Math.floor(bonus/price)
+    let ilePotrzeba =1
+    let ilePieter =0
+    for(let i=1;ilePuszek>=ilePotrzeba;i++)
+    {
+        ilePotrzeba = i*i
+        if(i===1) {
+            ilePuszek -= ilePotrzeba
+            ilePieter ++
+            ilePotrzeba = (i+1)*(i+1)
+        }
+        else
+        {
+            ilePuszek -= ilePotrzeba
+            ilePieter ++
+            ilePotrzeba = (i+1)*(i+1)
+        }
 
+    }
+    return ilePieter
+}
+// Returns number of complete beeramid levels
+var beeramid = function(bonus, price) {
+    var beers = Math.floor(bonus / price), levels = 0;
+    while(beers >= ++levels * levels) {
+        beers -= levels * levels;
+    }
+    return levels - 1;
 }
