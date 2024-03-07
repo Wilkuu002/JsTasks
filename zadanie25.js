837594a8517daeb915c2c0c897485f493e2cea52
Wilkuu002 <Kacperwilk@poczta.onet.pl>
2024-03-08 00:17:18 +0100

    zadaniejs 07.03 dzial juz cale

--- zadanie25.js
+++ zadanie25.js
@@ -1,24 +1,19 @@
 Array.prototype.sameStructureAs = function (other) {
-    // Return 'true' if and only if 'other' has the same
-    // nesting structure as 'this'.
-
-    // Note: You are given a function isArray(o) that returns
-    // whether its argument is an array.
-
     if (this.length !== other.length) {
         return false;
     }
-        let TakaSama =true
-        for(let i=0;i<this.length;i++)
-        {
-            const czyTakieSamoArray= typeof(this[i])
-            const czyTakieSamoOther = typeof (other[i])
 
-            if(czyTakieSamoArray !== czyTakieSamoOther)
-            {
-                TakaSama = false
-                break;
+    for (let i = 0; i < this.length; i++) {
+        if (Array.isArray(this[i]) !== Array.isArray(other[i])) {
+            return false;
+        }
+
+        if (Array.isArray(this[i]) && Array.isArray(other[i])) {
+            if (!this[i].sameStructureAs(other[i])) {
+                return false;
             }
         }
-        return TakaSama
-}
+    }
+
+    return true;
+};
