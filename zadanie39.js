21d1dc9d4db62ab0f51912307fd72bfd722c79d5
Wilkuu002 <Kacperwilk@poczta.onet.pl>
2024-07-29 17:32:44 +0200

    zadania

--- zadanie39.js
+++ zadanie39.js
@@ -1,3 +1,23 @@
-var uniqueInOrder=function(iterable){
-
+function isValidWalk(walk) {
+    let x=0
+    let y =0
+    if(walk.length !== 10) return false
+    for (let i=0;i<walk.length;i++)
+    {
+        switch (walk[i]) {
+            case 'n':
+                x++;
+                break
+            case 's':
+                x--
+                break
+            case 'e':
+                y++
+                break
+            case 'w':
+                y--
+                break
+        }
+    }
+    if(x===0 && y===0) return true
 }
